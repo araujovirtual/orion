6ddffa028a0f0e2b31aaa77ca42db4e5dd5ae678
araujovirtual <araujovirtual@gmail.com>
2021-09-09 17:27:10 -0300

    teste.py

--- teste.py
+++ teste.py
@@ -27,10 +27,24 @@ for l in lista:
             # insere os dados caso o filtro tenha sido aplciado
             dados.append(x.group())
 
+# imagem de fundo
+
+
 
 # Cria uma subplot
 fig, ax = plt.subplots()
+fig.set_size_inches(10.5, 5.5)
+fig.canvas.manager.set_window_title('ORION')
+fig.patch.set_facecolor('#E0E0E0')
+fig.patch.set_alpha(0.7)
+# Configuração da figura plot
+
+# 
 plt.subplots_adjust(bottom=0.35)
+plt.title("ORION")
+ax.set_ylabel("Balança")
+ax.set_xlabel("Tempo")
+
 
 # iterações e tempos inicias
 iteracoes = 0
@@ -39,10 +53,15 @@ tempo = len(dados)
 # Cria os axes para os sliders
 tt = plt.axes([0.125, 0.1, 0.78, 0.03])
 ii = plt.axes([0.125, 0.2, 0.78, 0.03])
+rs = plt.axes([0.8, 0.025, 0.1, 0.04])
 
 # Cria o slider propriamente dito
 t = Slider(tt, "Tempo", 1.0, len(dados))
 i = Slider(ii, "Iterações", 0.0, len(dados))
+
+# Botão de reset
+bt = Button(rs, 'Resetar', color='gold',
+                hovercolor='skyblue')
 
 
 # Função para atualizar os valores
@@ -53,12 +72,22 @@ def update(val):
     #print(int(tempo))
     xx = [float(dados[x][3:])
           for x in range(int(iter), len(dados), int(tempo))]
+    ax.set_ylabel("Balança")
+    ax.set_xlabel("Tempo")
     ax.plot(xx)
+    
 
+# Função para resetar
+def reset(event):
+    t.reset()
+    i.reset()
+    ax.clear()
+    ax.set_ylabel("Balança")
+    ax.set_xlabel("Tempo")
 
 # Captura o evento do click ou da mudança
 t.on_changed(update)
 i.on_changed(update)
-
+bt.on_clicked(reset)
 # Plota o gráfico
 plt.show()
